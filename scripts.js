5bc7faad04b7780ff4e9485676d4ce61c28a436a
fanerotim <61364731+fanerotim@users.noreply.github.com>
2025-10-25 21:35:08 +0300

    add logic that handles the custom select functionality

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // get custom select element
 const customSelectBtn = document.querySelector('.custom__select');
 
-// get custom options wrapper
-const customOptionsWrapper = document.querySelector('.options__wrapper');
-
 // add event listener to custom select element, so we can show hide the options
 customSelectBtn.addEventListener('click', customSelectClickHandler)
+
+// get custom options wrapper
+const customOptionsWrapper = document.querySelector('.options__wrapper');
 
 // function that handles open and close state of custom select
 function customSelectClickHandler() {
@@ -18,4 +18,39 @@ function customSelectClickHandler() {
         customOptionsWrapper.classList.remove('show');
         customOptionsWrapper.classList.add('hide');
     }
+}
+
+// get all options in a NodeList
+const optionElements = document.querySelectorAll('.option');
+
+// attach an event listener to each option
+optionElements.forEach(o => o.addEventListener('click', handleOptionChange))
+
+// custom__select paragraph
+let customSelectElement = document.querySelector('.custom__select');
+
+// change custom select value upon option change
+function handleOptionChange() {
+    // update custom__select paragraph to match the value of the selected / clicked option
+    // 1. get value of current select option
+    const currentOption = this.textContent;
+    // update value of custom select
+    customSelectElement.textContent = currentOption;
+
+    // call customSelectClickHandler to close the option list
+    customSelectClickHandler();
+}
+
+// handle form submission
+
+// get submit button
+const formSubmitButton = document.querySelector('.submit__btn');
+
+// add event listener to the submit button
+formSubmitButton.addEventListener('click', submitFormHandler)
+
+function submitFormHandler(e) {
+    e.preventDefault();
+    // log value of selected option - it is basically the textContent of customSelectElement
+    console.log(customSelectElement.textContent);
 }
